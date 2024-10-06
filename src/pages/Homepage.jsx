9436974b611f26132a67a941bd18d642b8667f00
Stanley-enumahh <enumah44@gmail.com>
2024-10-06 12:33:16 +0300

    home contd

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -30,36 +30,36 @@ const Hero1 = () => {
 
 const HeroContent = () => {
   return (
-    <div className="h-[100%] relative z-50 w-full flex justify-center items-center">
-      <div className="h-full w-full flex-col flex  justify-center items-center gap-[70px] md:gap-[90px]">
+    <div className="h-full relative z-50 w-full flex justify-center items-center">
+      <div className="h-full w-full flex-col flex  md:justify-center justify-start items-center gap-[60px] md:gap-[90px]">
         {/* images  start*/}
-        <div className=" w-full h-[40%] md:h-fit flex justify-center items-center relative">
-          <div className="h-[245px] md:h-[270px] w-[180px] md:w-[200px] bg-white rounded-2xl mr-[80px] overflow-hidden flex flex-col gap-4">
+        <div className=" w-full h-[30%] md:h-fit flex justify-center items-center relative md:mt-0 mt-[150px]">
+          <div className="h-[160px] md:h-[270px] w-[118px] md:w-[200px] bg-white rounded-2xl mr-[80px] overflow-hidden flex flex-col md:gap-4 gap-3 heroImage">
             <img
               src={HeroImage1}
               alt=""
-              className="h-[200px] md:h-[221px] w-full object-cover"
+              className="h-[130px] md:h-[221px] w-full object-cover"
             />
-            <div className="flex flex-row gap-3 w-full px-5">
-              <p className="bg-[#EC0402] w-[19px] h-[19px] rounded-full"></p>
-              <span className="flex flex-col gap-2">
-                <p className="bg-[#F5F5F5] w-[100px] h-[6px] rounded-lg"></p>
-                <p className="bg-[#F5F5F5] w-[57px] h-[6px] rounded-lg"></p>
+            <div className="flex flex-row gap-3 w-full px-2">
+              <p className="bg-[#EC0402] w-[11px] h-[11px] md:w-[19px] md:h-[19px] rounded-full"></p>
+              <span className="flex flex-col gap-1 md:gap-2">
+                <p className="bg-[#F5F5F5] w-[59px] md:w-[100px] h-[4px] md:h-[6px] rounded-lg"></p>
+                <p className="bg-[#F5F5F5] w-[33px] md:w-[57px] h-[4px] md:h-[6px] rounded-lg"></p>
               </span>
             </div>
           </div>
         </div>
-        <div className="md:h-[194px] w-[140px] h-[170px] md:w-[164px] bg-white p-[2px] rounded-lg absolute md:top-[360px] top-[34%] left-[52%] md:left-[730px]">
+        <div className="md:h-[194px] w-[97px] h-[115px] md:w-[164px] bg-white p-[2px] rounded-lg absolute md:top-[360px] top-[35%] left-[49%] md:left-[730px] shadow-xl heroImage">
           <img src={HeroImage2} alt="" className="h-full w-full object-cover" />
         </div>
         {/* images  end*/}
         {/* text start */}
-        <div className=" w-[100%] md:w-[50%] flex flex-col items-center justify-center text-center md:gap-3">
-          <h1 className="text-2xl md:text-4xl font-bold text-[#737070]">
+        <div className=" w-[95%] md:w-[50%] flex flex-col items-center justify-center text-center md:gap-3 ">
+          <h1 className="text-xl md:text-4xl font-bold text-[#737070]">
             <span className="text-black text-nowrap"> Your Moments,</span>{" "}
             <br /> Our Masterpieces in Every Frame.
           </h1>
-          <p className="text-xs font-semibold">
+          <p className="text-xs md:font-semibold">
             Easily browse our availability and book your session in just few
             clicks, with exclusive deals waiting for you. Get instant
             confirmation and secure your spot hassle free{" "}
@@ -69,7 +69,7 @@ const HeroContent = () => {
           </button>
         </div>
 
-        <div className="h-[50px] w-[85%] md:w-[530px] absolute bottom-0 hero-icon-section bg-white flex flex-row justify-around">
+        <div className="h-[50px] w-[85%] md:w-[530px] absolute bottom-0 hero-icon-section bg-white flex flex-row justify-around px-2">
           <div className="w-[160px] items-center justify-between gap-3 flex flex-row">
             <img src={FilmIcon} alt="" />
             <p className="text-xs font-semibold">
